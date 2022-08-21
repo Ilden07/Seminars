4425defc21aedbe6dfd188e7447df7db58cf93d3
Artem <22ilden07@gmail.com>
2022-08-21 15:15:51 +0700

    Task #1

--- Seminar (С#)/Seminar001_(C#)/Program.cs
+++ Seminar (С#)/Seminar001_(C#)/Program.cs
@@ -1,1 +1,12 @@
-﻿Initialization
+﻿
+/* Задача №1. Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat = number * number;
+
+Console.WriteLine("The quad of " + number + " is " + kvadrat);
+
+Console.WriteLine($"The quad of {number} is {kvadrat}");
+*/
